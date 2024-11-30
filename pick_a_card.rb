95ab825d306de41b945345047941bc0a42a9f0e4
marus0v <andrey.marus@gmail.com>
2024-11-30 18:24:53 +0300

    changes

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,10 @@
+require 'unicode'
+
 # Массивы с наборами мастей и достоинств
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Крести, Пики
+# suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Крести, Пики
+suits = [U+2660, U+2661, U+2662, U+2663] # Буби, Черви, Крести, Пики
 
 # Выведем произвольную карту, выбрав по одному элементу из массивов
-puts "#{values.sample} of #{suits.sample}"
+# puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample}"
